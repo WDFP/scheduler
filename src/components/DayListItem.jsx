d6ccea1cb8d52efd117bd0dea81e3f90493ad0ed
Victor Ha <victorha88@gmail.com>
2023-03-11 17:23:27 -0500

    Testing the DayListItem component

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import classNames from "classnames";
 import "./DayListItem.scss";
 
+const formatSpots = (spots) => {
+  if (spots=== 0) {
+    return "no spots remaining";
+  } else if  (spots=== 1) {
+    return "1 spot remaining";
+  } else {
+  return `${spots} spots remaining`;
+  }
+}
+
 const DayListItem = (props) => {
   const dayClass = classNames("day-list__item", {
     "day-list__item": props.item,
@@ -12,7 +22,7 @@ const DayListItem = (props) => {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
     <h2 className="text--regular">{props.name}</h2>
-    <h3 className="text--light">{props.spots} spots remaining</h3>
+    <h3 className="text--light">{formatSpots(props.spots)}</h3>
   </li>
   );
 }
