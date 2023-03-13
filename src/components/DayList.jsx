b4ff08dde731f6d0c1303019f407325137156bdd
Victor Ha <victorha88@gmail.com>
2023-03-13 19:27:52 -0400

    Build the InterviewerList component

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import DayListItem from './DayListItem'
+import React from "react";
+import DayListItem from "./DayListItem";
 
 const DayList = (props) => {
-
-  const DayListItems = props.days.map(item => {
+  const DayListItems = props.days.map((item) => {
     return (
       <DayListItem
-      key={item.id}
-      name={item.name} 
-      spots={item.spots} 
-      selected={item.name === props.day}
-      setDay={props.setDay}  
+        key={item.id}
+        name={item.name}
+        spots={item.spots}
+        selected={item.name === props.day}
+        setDay={props.setDay}
       />
     );
-  })
+  });
 
-  return (
-    <ul>{DayListItems}</ul>
-  )
+  return <ul>{DayListItems}</ul>;
 };
 
 export default DayList;
