6728684fab95576ed7d4d5b6b5501fa847e8ba86
Victor Ha <victorha88@gmail.com>
2023-03-15 13:42:30 -0400

    Implement retreiving Appointments with Promise.all() in useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,45 +6,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const appointments = {
-  1: {
-    id: 1,
-    time: "12pm",
-  },
-  2: {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: "2pm",
-  },
-  4: {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: "4pm",
-  },
-};
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application() {
   const setDay = (day) => setState({ ...state, day });
@@ -55,9 +17,14 @@ export default function Application() {
     appointments: {},
   });
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
   useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      setState((prev) => ({ ...prev, days: response.data }));
+    Promise.all([
+    axios.get("/api/days"),
+    axios.get("/api/appointments")
+    ]).then((all) => {
+      setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data }));
     });
   }, []);
 
@@ -80,7 +47,7 @@ export default function Application() {
         />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((appointment) => {
+        {dailyAppointments.map((appointment) => {
           return (
             <Appointment
               key={appointment.id}
